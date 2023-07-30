dca7358f595ce389254a0a2a9b134df0faa3e8b8
Augustors <augusto7410@gmail.com>
2023-07-30 16:50:59 -0300

    First_Commit

---
+++ Produto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace StockProject
+{
+    internal class Produto
+    {
+        public string? Name { get; set; }
+        public double Preco { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Produto()
+        {
+            Quantity = 10;
+        }
+
+        public Produto(string? name, double preco) : this()
+        {
+            Name = name;
+            Preco = preco;
+        }
+
+        public Produto(string? name, double preco, int quantity) : this(name, preco)
+        {
+
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantity;
+        }
+
+        public void AdicionarProdutos(int qtd)
+        {
+            Quantity += qtd;
+        }
+
+        public void RemoverProdutos(int qtd)
+        {
+            Quantity -= qtd;
+        }
+
+        public override string ToString()
+        {
+            return Name 
+                + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " 
+                + Quantity
+                + " unidades, Total: $" 
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using StockProject;
+using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] p = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine());
+                p[i] = new Produto(name, preco);
+            }
+
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += p[i].Preco;
+            }
+            Console.WriteLine(sum/n);
+
+        }
+    }
+}
